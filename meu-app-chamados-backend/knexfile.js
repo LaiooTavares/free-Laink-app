0335d3edc494c843aaca3abb4962fa158ee65305
LaiooTavares <laiotavares.souza@gmail.com>
2025-09-28 13:29:57 -0300

    subindo site 1.0.1

--- meu-app-chamados-backend/knexfile.js
+++ meu-app-chamados-backend/knexfile.js
@@ -3,9 +3,6 @@ const path = require('path');
 require('dotenv').config({ path: path.resolve(__dirname, '.env') });
 
 module.exports = {
-  /**
-   * Configuração para o ambiente de Desenvolvimento (sua máquina local).
-   */
   development: {
     client: 'pg',
     connection: {
@@ -13,7 +10,7 @@ module.exports = {
       port: process.env.DB_PORT,
       user: process.env.DB_USER,
       password: process.env.DB_PASSWORD,
-      database: process.env.DB_DATABASE, // CORRIGIDO de DB_NAME para DB_DATABASE
+      database: process.env.DB_DATABASE,
     },
     migrations: {
       directory: path.resolve(__dirname, 'src', 'database', 'migrations')
@@ -23,10 +20,6 @@ module.exports = {
     }
   },
 
-  /**
-   * Configuração para o ambiente de Produção (Easypanel, Render, etc.).
-   * ATUALIZADO para usar as mesmas variáveis separadas do ambiente de desenvolvimento.
-   */
   production: {
     client: 'pg',
     connection: {
@@ -34,7 +27,9 @@ module.exports = {
       port: process.env.DB_PORT,
       user: process.env.DB_USER,
       password: process.env.DB_PASSWORD,
-      database: process.env.DB_DATABASE, // CORRIGIDO de DB_NAME e ajustado para produção
+      database: process.env.DB_DATABASE,
+      // --- LINHA ADICIONADA PARA LIDAR COM SSL EM PRODUÇÃO ---
+      ssl: { rejectUnauthorized: false },
     },
     migrations: {
       directory: path.resolve(__dirname, 'src', 'database', 'migrations')
