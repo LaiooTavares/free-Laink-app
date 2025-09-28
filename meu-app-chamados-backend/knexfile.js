c2af48370789b1133c303e88808cb4d85710818f
LaiooTavares <laiotavares.souza@gmail.com>
2025-09-28 13:31:19 -0300

    subindo site 1.0.2

--- meu-app-chamados-backend/knexfile.js
+++ meu-app-chamados-backend/knexfile.js
@@ -20,17 +20,11 @@ module.exports = {
     }
   },
 
+  // --- CONFIGURAÇÃO DE PRODUÇÃO CORRIGIDA ---
+  // Voltamos a usar uma única DATABASE_URL, que é o padrão e o mais confiável.
   production: {
     client: 'pg',
-    connection: {
-      host: process.env.DB_HOST,
-      port: process.env.DB_PORT,
-      user: process.env.DB_USER,
-      password: process.env.DB_PASSWORD,
-      database: process.env.DB_DATABASE,
-      // --- LINHA ADICIONADA PARA LIDAR COM SSL EM PRODUÇÃO ---
-      ssl: { rejectUnauthorized: false },
-    },
+    connection: process.env.DATABASE_URL, // <-- A MUDANÇA ESTÁ AQUI
     migrations: {
       directory: path.resolve(__dirname, 'src', 'database', 'migrations')
     },
